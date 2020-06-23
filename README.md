c127b9235d125c2c93a76d987721372d49974a4c
Alessandro Melo <alessandro.melo2702@gmail.com>
2020-06-23 18:27:10 -0300

    correção de bugs #DEVMOBAILEFDS

--- README.md
+++
@@ -1,5 +0,0 @@
-# ProjetoFaculdade
-
-Observações: Para a funcionalidade de GPS funcionar, é necessário habilitar as permissões de acesso ao GPS antes de abrir o aplicativo.
-
-A agenda tem uma tela de loading, basta sair e voltar novamente que irá abrir!
--- src/browser/index.js
+++ src/browser/index.js
@@ -1,10 +1,84 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-export default function index() {
+import React, {useState} from 'react';
+import { View, TextInput, Text, StyleSheet } from 'react-native';
+import { RectButton} from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import { WebView } from 'react-native-webview';
+const Navegador = () => {
+    const [y, setY] = useState(String);
+    const navigation = useNavigation();
+    function hundleVoltar(){
+        navigation.goBack();
+    }
+    function browser(){
+        setY(campoUrl);
+    }
+    const [campoUrl, setCampoUrl] = useState('https://'); 
     return (
-        <View>
-            <Text>Browser</Text>
-        </View>
-    )
-}
+    <>
+    <WebView source={{uri: y}}/>
+    <View style={styles.container}>
+    <RectButton style={styles.button} onPress={hundleVoltar}>
+        <Text style={styles.buttonText}>
+            Voltar
+        </Text>
+    </RectButton>
+    <View style={styles.campoContainer}>
+    <TextInput style={styles.campo} 
+    value={campoUrl}
+    onChangeText={text => setCampoUrl(text)}
+    />
+    </View>
+    <RectButton style={styles.button} onPress={browser}>
+        <Text style={styles.buttonText}>
+            ir
+        </Text>
+    </RectButton>
+    </View>
+    
+    </>
+    )}
+    const styles = StyleSheet.create({
+        campoContainer: {
+            marginTop: 0,
+        },
+        campo:{
+            backgroundColor: '#3c3382',
+            color: '#FFF',
+            height: 50,
+            width: "95%",
+            borderColor: '#FFF',
+            borderRadius: 10,
+            textAlign: 'center',
+            marginBottom: 8,
+            fontSize:20,
+            marginLeft: 0,
+            marginTop: 1,
+            marginRight: 15,
+        },
+        container:{
+            width: '100%',
+            flexDirection: 'row',
+        },
+    
+        button: {
+            backgroundColor: '#3c3382',
+            height: 50,
+            width: "15%",
+            flexDirection: 'row',
+            borderRadius: 10,
+            overflow: 'hidden',
+            alignItems: 'center',
+            marginTop: 1,
+            marginLeft: 5,
+            marginRight: 5,
+        },
+        buttonText: {
+            flex: 1,
+            justifyContent: 'center',
+            textAlign: 'center',
+            color: '#FFF',
+            fontSize: 15,
+        },
+    })
+
+export default Navegador;
