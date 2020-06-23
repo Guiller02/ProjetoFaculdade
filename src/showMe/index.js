f7468df9fcf3d92c5d38027f4401061c4b6bd8c3
Alessandro Melo <alessandro.melo2702@gmail.com>
2020-06-22 22:36:08 -0300

    add show me on map function

--- src/showMe/index.js
+++ src/showMe/index.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapboxGL from "@react-native-mapbox-gl/maps";
 
-export default function index() {
-    return (
-        <View>
-            <Text>Show Me</Text>
-        </View>
-    )
+
+MapboxGL.setAccessToken('pk.eyJ1IjoiZXZvcmVzdCIsImEiOiJja2JyNDE0eDYwM2p3MnNxeHVydHBrMjNpIn0.nTH8Py47Ql9nTY_6w1uSjw');
+
+export default class App extends Component {
+    render() {
+        return (
+            <MapboxGL.MapView
+                showUserLocation={true}
+                style={{ flex: 1 }}
+                userTrackingMode={1}
+                styleURL={MapboxGL.StyleURL.Dark}
+            >
+        <MapboxGL.Camera
+            followUserLocation
+            zoomLevel={12}
+            followUserMode="course"
+            pitch={60}        
+        />
+        <MapboxGL.UserLocation />
+        </MapboxGL.MapView>
+        );
+      }
 }
