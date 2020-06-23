b6f600461e64aca361cfd98e756c29fe01d8a5b3
Alessandro Melo <alessandro.melo2702@gmail.com>
2020-06-23 18:38:41 -0300

    requisição de GPS funcionando

--- src/showMe/index.js
+++ src/showMe/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import MapboxGL from "@react-native-mapbox-gl/maps";
-
+import {request, PERMISSIONS} from 'react-native-permissions';
 
 MapboxGL.setAccessToken('pk.eyJ1IjoiZXZvcmVzdCIsImEiOiJja2JyNDE0eDYwM2p3MnNxeHVydHBrMjNpIn0.nTH8Py47Ql9nTY_6w1uSjw');
+
+
+request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then((result) => {
+    console.log('eae prof');
+});
 
 export default class App extends Component {
     render() {
